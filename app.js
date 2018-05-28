085af463bdccab448621c856a3c04d988521b1e9
John Blaine <jablaine@uw.edu>
2018-05-28 11:32:37 -0600

    Add list to index.html, add color change interactivity

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 /*jshint esversion: 6 */
 
-const myHeading = document.getElementById('myHeading');
+const myHeading = document.getElementsByTagName('h1')[0];
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
+const myList = document.getElementsByTagName('li');
 
 myButton.addEventListener('click', () => {
   myHeading.style.color = myTextInput.value;
+  for (let i = 0; i < myList.length; i++) {
+    myList[i].style.color = myTextInput.value;
+  }
 });
