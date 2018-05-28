d12f987c608ca4d1f71ad3d2d77012a4a6a9b021
John Blaine <jablaine@uw.edu>
2018-05-28 11:41:12 -0600

    Add to list and set new items to red on click

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 const myList = document.getElementsByTagName('li');
 
+const errorNotBlue = document.getElementsByClassName('error-not-blue');
+
 myButton.addEventListener('click', () => {
   myHeading.style.color = myTextInput.value;
   for (let i = 0; i < myList.length; i++) {
     myList[i].style.color = myTextInput.value;
+  }
+  for (let j = 0; j < errorNotBlue.length; j++) {
+    errorNotBlue[j].style.color = 'red';
   }
 });
